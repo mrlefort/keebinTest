9dd4666cbd662d611a82dc6378f2ae251a55503a
Kasper Roland Pagh <Kasper Roland Pagh>
2016-10-14 14:25:31 +0200

    initial git fuckup - sorry, glemte branch

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var facade = require("../JS/DataBaseFacade.js");
+var crypto = require('password-hash-and-salt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/*
+JEG HAR BRUG FOR AT FACADEN GIVER EN RETURN VALUE, SÅ JEG KAN SE OM JEG SKAL RETURNE HTTPKODE 200 ELLER ERROR!!!!!
+
+Also User tabel skal updates til at indeholde PassWord!!!! But this shit twerks, hvis bare tabellen var rigtig :)))
+ */
+router.post("/user/new", function(req, res, next)
+{
+
+  var pw;
+  crypto('mysecret').hash(function(error, hash)
+  {
+    if(error)
+      throw new Error('Something went wrong!');
+
+    // Store hash (incl. algorithm, iterations, and salt)
+    pw = hash;
+
+  //   // Verifying a hash
+  //   crypto('asdf').verifyAgainst(pw, function(error, verified)
+  //   {
+  //     if(error)
+  //       throw new Error('Something went wrong!');
+  //     if(!verified) {
+  //       console.log("Don't try! We got you!");
+  //     } else {
+  //       console.log("The secret is...");
+  //     }
+  //   });
+    console.log("her er pw: " +  pw);
+    var userToSave =
+    {
+      "firstName": req.body.firstName,
+      "lastName": req.body.lastName,
+      "email":req.body.email,
+      "role" :req.body.role ,
+      "birthday" : new Date(req.body.birthday),
+      "sex": req.body.sex,
+      "password" :pw
+    }
+    facade.newUser(userToSave);
+    res.status(200).send();
+  });
+
+
 });
 
 module.exports = router;
